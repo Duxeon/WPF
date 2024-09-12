edb8d9d81e48a3f067cc22158e3a890342cc67ab
Duxeon <duxeon000@gmail.com>
2024-09-12 15:47:01 +0200

    FCT

---
+++ MatchingGame_Mathias/MatchingGame_Mathias/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MatchingGame_Mathias
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tempsEcoule = 0;
+        int nbPairesTrouvees = 0;
+        public MainWindow()
+        {
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+
+            timer.Tick += new EventHandler(Timer_Tick);
+            InitializeComponent();
+
+            SetUpGame();
+
+        }
+
+        public void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+                {
+                    "🐈","🐈",
+                    "🐷","🐷",
+                    "🐐","🐐",
+                    "🦊","🦊",
+                    "🐴","🐴",
+                    "🦨","🦨",
+                    "🦉","🦉",
+                    "🐀","🐀",
+                };
+            int index;
+            Random nbAlea = new Random();
+            string nextEmoji;
+            foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "txtTemps")
+                {
+                    index = nbAlea.Next(animalEmoji.Count);
+                    nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+
+        }
+        TextBlock derniereTBClique;
+        bool trouvePaire = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlockActif = sender as TextBlock;
+            if (!trouvePaire)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                derniereTBClique = textBlockActif;
+                trouvePaire = true;
+            }
+            else if (textBlockActif.Text == derniereTBClique.Text)
+            {
+                textBlockActif.Visibility = Visibility.Hidden;
+                trouvePaire = false;
+            }
+            else
+            {
+                derniereTBClique.Visibility = Visibility.Visible;
+                trouvePaire = false;
+            }
+
+        }
+    }
+}
