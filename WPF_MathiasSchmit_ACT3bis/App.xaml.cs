8dd146657df9b64f4d453347990540b5d66b42da
Duxeon <duxeon000@gmail.com>
2024-10-07 17:59:15 +0200

    grbfv

---
+++ WPF_MathiasSchmit_ACT3bis/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_MathiasSchmit_ACT3bis
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
